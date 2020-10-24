9ae164f2c61b7c64ea6eaf91819a0f35b9cf1cec
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-10-24 14:16:46 +0400

    Added code changes for video2

--- main.go
+++ main.go
@@ -3,16 +3,29 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
-	go process("order")
-	process("refund")
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+	    defer wg.Done()
+	    process("order")
+	}()
+
+	go func() {
+	    defer wg.Done()
+	    process("refund")
+	}()
+	wg.Wait()
+
+	process("cancel")
 }
 
 func process(item string) {
-	for i := 1; true; i++ {
+	for i := 1; i <= 5; i++ {
 		fmt.Println("Processed", i, item)
-		time.Sleep(time.Second / 2) // 0.5
+		time.Sleep(time.Second) // 0.5
 	}
 }
--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import asyncio
 
 
 async def async_generator():
-    for i in range(1, 100000):
+    for i in range(1, 6):
         yield i
-        await asyncio.sleep(0.5)
 
 
 async def process(item):
     async for i in async_generator():
         print("Processed", i, item)
+        await asyncio.sleep(1)
 
 
 async def main():
     task1 = asyncio.create_task(process("order"))
     task2 = asyncio.create_task(process("refund"))
 
-    await task1
-    await task2
+    # One possible way
+    # await task1
+    # await task2
+    # await process("cancel")
+
+    # Second more understandable way
+    tasks = [task1, task2]
+    await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    await process("cancel")
 
 
 if __name__ == "__main__":
