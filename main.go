fca81a9811851a68ebb07ae48a86b1d5e55e9038
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-10-27 17:56:14 +0400

    Added done channel to exit from for loop

--- main.go
+++ main.go
@@ -8,29 +8,36 @@ import (
 func main() {
     out1 := make(chan string)
     out2 := make(chan string)
+    done := make(chan int)
 
     go func(){
-        for i := 0; i <=5; i++ {
+        for i := 1; i <=5; i++ {
             time.Sleep(time.Second / 2)
             out1 <- "order processed"
         }
+        done <- 1
     }()
 
     go func() {
-        for i := 0; i <=5; i++ {
+        for i := 1; i <=5; i++ {
             time.Sleep(time.Second)
             out2 <- "refund processed"
         }
+        done <- 1
     }()
 
-    for  {
+    for n:=2; n>0;  {
         select {
             case msg := <- out1:
                 fmt.Println(msg)
             case msg := <- out2:
                 fmt.Println(msg)
+            case <-done:
+                n--
         }
-        }
+    }
+
+
 //         fmt.Println(<-out1)
 //         fmt.Println(<-out2)
 //     }
