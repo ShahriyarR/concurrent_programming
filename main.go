38eb0c1d77a2b01ce296736127b704d35260c286
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2020-10-31 12:08:42 +0400

    Code changes for video4 (#4)
    * Code changes for video3 is ready
    
    * Code changes for video4
    
    * Added done channel to exit from for loop

--- main.go
+++ main.go
@@ -6,19 +6,41 @@ import (
 )
 
 func main() {
-	c := make(chan string)
-	go process("order", c)
-	for item := range(c) {
-	    fmt.Println("Processed", item)
-	}
+    out1 := make(chan string)
+    out2 := make(chan string)
+    done := make(chan int)
+
+    go func(){
+        for i := 1; i <=5; i++ {
+            time.Sleep(time.Second / 2)
+            out1 <- "order processed"
+        }
+        done <- 1
+    }()
+
+    go func() {
+        for i := 1; i <=5; i++ {
+            time.Sleep(time.Second)
+            out2 <- "refund processed"
+        }
+        done <- 1
+    }()
+
+    for n:=2; n>0;  {
+        select {
+            case msg := <- out1:
+                fmt.Println(msg)
+            case msg := <- out2:
+                fmt.Println(msg)
+            case <-done:
+                n--
+        }
+    }
+
+
+//         fmt.Println(<-out1)
+//         fmt.Println(<-out2)
+//     }
 
 }
 
-func process(item string, c chan string) {
-	for i := 1; i <= 5; i++ {
-	    fmt.Println("Put the", item)
-	    c <- item
-		time.Sleep(time.Second / 2) // 0.5
-	}
-	close(c)
-}
--- main.py
+++ main.py
@@ -7,20 +7,30 @@ async def async_generator():
         yield i
 
 
-async def process(item, c):
+
+async def process(item, c, sleep_time=0.5):
     async for _ in async_generator():
         await c.put(item)
-        print(f"Put the {item}")
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(sleep_time)
+
+
+async def process_channel(c, sleep_time=0.5):
+    async for item in c:
+        await asyncio.sleep(sleep_time)
+        print(item)
 
 
 async def main():
-    c = Chan()
-    Goroutine.go(process("order", c))
-    await asyncio.sleep(0.5)
-    async for item in c:
-        print("Processed", item)
-        await asyncio.sleep(0.5)
+    c1 = Chan()
+    c2 = Chan()
+    Goroutine.go(process("order", c1, sleep_time=0.5))
+    Goroutine.go(process("refund", c2, sleep_time=1))
+
+    task1 = asyncio.create_task(process_channel(c1, sleep_time=0.5))
+    task2 = asyncio.create_task(process_channel(c2, sleep_time=1))
+    await task2
+    await task1
+
 
 
 if __name__ == "__main__":
